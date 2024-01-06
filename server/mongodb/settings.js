0649454abd44240c1bb1dcafff11775d5a3aec90
tzuminglu <1994515daniel@gmail.com>
2024-01-06 18:47:21 -0500

    update mongodb settings.js file
    put username and password in .env file since the security issues

--- server/mongodb/settings.js
+++ server/mongodb/settings.js
@@ -1,14 +1,12 @@
 // the first one url address is the web server, the second one is local server
+import dotenv from "dotenv";
+dotenv.config();
+
 const mongoURL =
-  "mongodb+srv://user01:wrWCBeX1WtssST6J@cluster0.ja5dufq.mongodb.net/" ||
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ja5dufq.mongodb.net/` ||
   "mongodb://127.0.0.1:27017/";
 
 export const mongoConfig = {
   serverUrl: mongoURL,
   database: "TAANJ",
 };
-
-// export const mongoConfig = {
-//   serverUrl: 'mongodb://localhost:27017/',
-//   database: 'apiBasedBlog'
-// };
