a099a774d0d312c2636d530bdd7f9bb26cdf0028
tzuminglu <1994515daniel@gmail.com>
2024-01-07 19:03:15 -0500

    update heroku

--- server/app.js
+++ server/app.js
@@ -2,27 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import configRoutes from "./routes/index.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 // import { createClient } from "redis";
 // import asyncRedis from "async-redis";
-
 
 dotenv.config();
 // const client = asyncRedis.createClient(process.env.REDISTOGO_URL);
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World! 123')
-})
+app.get("/", (req, res) => {
+  console.log(path.join(__dirname));
+  res.send("Hello World! 123");
+});
 
 // routes for upcoming event
 app.use("/admin/upcomingevent", async (req, res, next) => {
-  next()
+  next();
 });
-
 
 configRoutes(app);
 
