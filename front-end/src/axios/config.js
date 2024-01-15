3a8e2316a1baceb9062b8fb581f6d497c8d195ed
tzuminglu <1994515daniel@gmail.com>
2024-01-15 16:58:10 -0500

    fix axios cors error

--- front-end/src/axios/config.js
+++ front-end/src/axios/config.js
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_APP_API_URL;
 
-const axiosClient = axios.create({ withCredentials: true, baseURL });
+const axiosClient = axios.create({
+  baseURL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
 
 export default axiosClient;
