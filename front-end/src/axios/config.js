44fb0415375ce1af135b3ec85edbcb72005a4c07
tzuminglu <1994515daniel@gmail.com>
2024-01-15 16:51:15 -0500

    update axios credentials

--- front-end/src/axios/config.js
+++ front-end/src/axios/config.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_APP_API_URL;
 
-const axiosClient = axios.create({ baseURL });
+const axiosClient = axios.create({ withCredentials: true, baseURL });
 
 export default axiosClient;
