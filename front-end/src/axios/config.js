244d98c2311b2da299e212f9d0cf25ad36198e77
tzuminglu <1994515daniel@gmail.com>
2024-01-15 17:34:58 -0500

    fix cors error

--- front-end/src/axios/config.js
+++ front-end/src/axios/config.js
@@ -5,7 +5,7 @@ const baseURL = import.meta.env.VITE_APP_API_URL;
 const axiosClient = axios.create({
   baseURL,
   headers: {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "https://taanj-client.vercel.app",
     "Content-Type": "application/json",
   },
   withCredentials: true,
